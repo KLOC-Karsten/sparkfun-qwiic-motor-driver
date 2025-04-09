9940399efb44dccd9f86469c1fecd6b32bbd8231
Karsten Lueth <kl@kloc-consulting.de>
2025-04-09 20:06:39 +0200

    First full implementation, not tested yet

--- src/sparkfun_qwiic_motor_driver.adb
+++ src/sparkfun_qwiic_motor_driver.adb
@@ -13,6 +13,19 @@ package body Sparkfun_Qwiic_Motor_Driver is
    --  https://github.com/sparkfun/
    --   SparkFun_Serial_Controlled_Motor_Driver_Arduino_Library
 
+   --  I2C command definitions
+   SCMD_ID            : constant UInt8 := 16#01#;
+   SCMD_MA_DRIVE      : constant UInt8 := 16#20#;
+   SCMD_DRIVER_ENABLE : constant UInt8 := 16#70#;
+   SCMD_STATUS_1      : constant UInt8 := 16#77#;
+
+   --  SCMD_STATUS_1 bits
+   SCMD_ENUMERATION_BIT  : constant UInt8 := 16#01#;
+   SCMD_BUSY_BIT         : constant UInt8 := 16#02#;
+   SCMD_REM_READ_BIT     : constant UInt8 := 16#04#;
+   SCMD_REM_WRITE_BIT    : constant UInt8 := 16#08#;
+   --  SCMD_HW_EN_BIT        : constant UInt8 := 16#10#;
+
    --  Write to I2C register.
    procedure Write_Register (Offset  : UInt8;
                              Data    : UInt8;
@@ -29,78 +42,85 @@ package body Sparkfun_Qwiic_Motor_Driver is
       Success := Status = HAL.I2C.Ok;
    end Write_Register;
 
-   --  I2C command definitions
-   Direction_Set     : constant UInt16 := 16#aa#;
-   Motor_Speed_Set   : constant UInt16 := 16#82#;
-   PWM_Frequence_Set : constant UInt16 := 16#42#;
-   Nothing           : constant UInt8 := 16#01#;
+   --  Read from I2C register.
+   procedure Read_Register (Offset  : UInt8;
+                            Data    : out UInt8;
+                            Success : out Boolean)
+   is
+      Status : HAL.I2C.I2C_Status;
+      Buffer : HAL.I2C.I2C_Data := (1 => 0);
+   begin
+      Port.Mem_Read (Addr          => I2C_Addr,
+                     Mem_Addr      => UInt16 (Offset),
+                     Mem_Addr_Size => Memory_Size_8b,
+                     Data          => Buffer,
+                     Status        => Status);
 
-   --  Prescaler Frequence
-   --  F_31372Hz  : constant UInt8 := 16#1#;
-   --  F_3921Hz   : constant UInt8 := 16#2#;
-   --  F_490Hz    : constant UInt8 := 16#3#;
-   --  F_122Hz    : constant UInt8 := 16#4#;
-   --  F_30Hz     : constant UInt8 := 16#5#;
+      Success := Status = HAL.I2C.Ok;
+      Data    := Buffer (1);
+   end Read_Register;
 
-   --  Sets the PWM frequency. Must by one of above values.
-   procedure Frequence (Addr    : HAL.I2C.I2C_Address;
-                        Freq    : UInt16;
+   procedure Set_Drive (Motor   : Motor_ID;
+                        Speed   : Motor_Speed;
                         Success : out Boolean)
    is
-      LSB : constant UInt8 := UInt8 (Freq and 16#ff#);
-      MSB : constant UInt8 := UInt8 (Shift_Right (Freq, 8) and 16#ff#);
-      Status : HAL.I2C.I2C_Status;
+      Offset : constant UInt8 := SCMD_MA_DRIVE + UInt8 (Motor);
+      Data   : constant UInt8 := UInt8 (127 + Speed);
    begin
-      Port.Mem_Write (Addr          => Addr,
-                      Mem_Addr      => PWM_Frequence_Set,
-                      Mem_Addr_Size => Memory_Size_8b,
-                      Data          => (1 => LSB, 2 => MSB),
-                      Status        => Status);
+      Write_Register (Offset, Data, Success);
+   end Set_Drive;
 
-      Success := Status = HAL.I2C.Ok;
-   end Frequence;
-
-   procedure Initialize (Addr    : HAL.I2C.I2C_Address;
+   procedure Set_Enable (Enable  : Boolean;
                          Success : out Boolean) is
    begin
-      Frequence (Addr, 16#0202#, Success);
+      if Enable then
+         Write_Register (SCMD_DRIVER_ENABLE, 1, Success);
+      else
+         Write_Register (SCMD_DRIVER_ENABLE, 0, Success);
+      end if;
+   end Set_Enable;
+
+   procedure Is_Busy (Busy    : out Boolean;
+                      Success : out Boolean)
+   is
+      Status : UInt8;
+      Flags  : constant UInt8 :=
+         SCMD_BUSY_BIT + SCMD_REM_READ_BIT + SCMD_REM_WRITE_BIT;
+   begin
+      Read_Register (SCMD_STATUS_1, Status, Success);
+      if Success then
+         if (Status and Flags) > 0 then
+            Busy := True;
+         else
+            Busy := False;
+         end if;
+      end if;
+   end Is_Busy;
+
+   procedure Is_Ready (Ready   : out Boolean;
+                       Success : out Boolean)
+   is
+      Status : UInt8;
+   begin
+      Read_Register (SCMD_STATUS_1, Status, Success);
+      if Success then
+         if Status = 16#ff# then
+            Ready := False;
+         elsif (Status and SCMD_ENUMERATION_BIT) > 0 then
+            Ready := True;
+         else
+            Ready := False;
+         end if;
+      end if;
+   end Is_Ready;
+
+   procedure Initialize (Ident   : out UInt8;
+                         Success : out Boolean) is
+   begin
+      --  Dummy Read
+      Read_Register (SCMD_ID, Ident, Success);
+
+      Read_Register (SCMD_ID, Ident, Success);
    end Initialize;
-
-   --  Sets the direction for both channels.
-   procedure Speed (Addr    : HAL.I2C.I2C_Address;
-                    Speed_1 : Motor_Speed;
-                    Speed_2 : Motor_Speed;
-                    Success : out Boolean)
-   is
-      Status : HAL.I2C.I2C_Status;
-   begin
-      Port.Mem_Write (Addr          => Addr,
-                      Mem_Addr      => Motor_Speed_Set,
-                      Mem_Addr_Size => Memory_Size_8b,
-                      Data          => (1 => UInt8 (Speed_1),
-                                        2 => UInt8 (Speed_2)),
-                      Status        => Status);
-
-      Success := Status = HAL.I2C.Ok;
-   end Speed;
-
-   --  Sets the direction for both channels.
-   procedure Direction (Addr        : HAL.I2C.I2C_Address;
-                        Direction_1 : Motor_Direction;
-                        Direction_2 : Motor_Direction;
-                        Success     : out Boolean)
-   is
-      Dir : constant UInt8 := Motor_Direction'Pos (Direction_2) * 4
-                              + Motor_Direction'Pos (Direction_1);
-      Status : HAL.I2C.I2C_Status;
-   begin
-      Port.Mem_Write (Addr          => Addr,
-                      Mem_Addr      => Direction_Set,
-                      Mem_Addr_Size => Memory_Size_8b,
-                      Data          => (1 => Dir, 2 => Nothing),
-                      Status        => Status);
-
-      Success := Status = HAL.I2C.Ok;
-   end Direction;
 
 end Sparkfun_Qwiic_Motor_Driver;
--- src/sparkfun_qwiic_motor_driver.ads
+++ src/sparkfun_qwiic_motor_driver.ads
@@ -14,19 +14,24 @@ package Sparkfun_Qwiic_Motor_Driver is
    type Motor_Direction is (CCW, CW);
    for Motor_Direction use (CCW => 1, CW => 2);
 
-   subtype Motor_Speed is HAL.UInt8 range 0 .. 100;
+   subtype Motor_ID is HAL.UInt8 range 0 .. 1;
 
-   procedure Initialize (Addr    : HAL.I2C.I2C_Address;
+   subtype Motor_Speed is Integer range -127 .. 127;
+
+   procedure Initialize (Ident   : out HAL.UInt8;
                          Success : out Boolean);
 
-   procedure Direction (Addr        : HAL.I2C.I2C_Address;
-                        Direction_1 : Motor_Direction;
-                        Direction_2 : Motor_Direction;
-                        Success     : out Boolean);
+   procedure Is_Ready (Ready   : out Boolean;
+                       Success : out Boolean);
 
-   procedure Speed (Addr    : HAL.I2C.I2C_Address;
-                    Speed_1 : Motor_Speed;
-                    Speed_2 : Motor_Speed;
-                    Success : out Boolean);
+   procedure Is_Busy (Busy    : out Boolean;
+                      Success : out Boolean);
+
+   procedure Set_Enable (Enable  : Boolean;
+                         Success : out Boolean);
+
+   procedure Set_Drive (Motor   : Motor_ID;
+                        Speed   : Motor_Speed;
+                        Success : out Boolean);
 
 end Sparkfun_Qwiic_Motor_Driver;
